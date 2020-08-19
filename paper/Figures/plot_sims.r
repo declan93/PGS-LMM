dd3c825a75d19a2aa4cb9e6705ab207cb823cbba
Declan Bennett <declanbennett@hotmail.com>
2020-08-19 12:46:54 +0100

    Update plot_sims.r

--- paper/Figures/plot_sims.r
+++ paper/Figures/plot_sims.r
@@ -77,3 +77,24 @@ pr3 <- ggplot(d,aes(x=x,y=y,colour=colour)) + theme_classic(base_size=10)+ scale
 panel_fig2 <- annotate_figure(ggarrange(pr3,pr1, nrow = 2,labels=c("A","B")),left=text_grob("\U0394 Sensitivity", color = "black", rot = 90,hjust = .2),bottom=text_grob("Specificity",hjust = 0.2))
 
 ggsave(pr2, filename = "dROC_PGS_LMM_VS_BOLT.png",dpi=350)
+
+
+## fig1a 
+read.table("fig1a.txt",h=T)
+p_arr <- ggplot(d, aes(x=fastGWA/1000, y=`fastGWA-PGS`/1000))+ geom_point() +geom_abline() +
+geom_hline(yintercept=median(d$"fastGWA-PGS"/1000), linetype="dotted", color="#7CAE00", size=1) +ylim(.4,.55) + xlim(.4,.48) + 
+geom_hline(yintercept=median(d$fastGWA/1000), linetype="dotted", color="grey", size=1) + xlab("fastGWA")  + ylab("fastGWA-PGS") + 
+theme_classic()
+
+## fig1b 
+read.table("fig1b.txt",h=T)
+p <- ggplot(c1, aes(x=variable, y=value/1000, colour=variable)) + geom_boxplot(outlier.shape=NA) + xlab("Method") + 
+ylab("Proportion of causal variants")+ theme(legend.position = "none") +
+geom_hline(yintercept=median(c1[c1$variable=="fastGWA",2]/1000), linetype="dotted", color="#00BFC4") +
+geom_hline(yintercept=median(c1[c1$variable=="Bolt-PGS",2]/1000), linetype="dotted", color="#7CAE00") +
+geom_hline(yintercept=median(c1[c1$variable=="Bolt",2]/1000), linetype="dotted", color="#F8766D") + 
+geom_hline(yintercept=median(c1[c1$variable=="fastGWA-PGS",2]/1000), linetype="dotted", color="#C77CFF") + theme_classic() + 
+theme(legend.position="none") + ylim(0.4,0.6)
+
+fig1 <- ggarrange(p_arr,p,nrow=2,labels=c("A","B"))
+ggsave("fig.png",fig1,dpi=350)
