d61500dbb5de32315eaf2621f04dc306817125ae
Declan Bennett <declanbennett@hotmail.com>
2021-04-27 12:31:41 +0100

    Update makePgsCovars.sh

--- source/makePgsCovars.sh
+++ source/makePgsCovars.sh
@@ -5,7 +5,7 @@ source "${PWD}/config.txt"
 for i in {1..22}; do
 	awk 'NR==FNR{a[$1]=$2;next}{print $0,a[$1]}' $age ${GT}/pca.eigenvec > ${traits}/qcovars2_${i}.txt
 	awk 'NR==FNR{a[$1]=$2;next}{print $0,a[$1] ? a[$1] : "NA"}' $batch ${traits}/qcovars2_${i}.txt > ${traits}/qcovars3_${i}.txt
-	#awk 'NR==FNR{a[$1]=$5;next}{print $0,a[$1] ? a[$1] : "NA"}' ${PGS}/${i}.sscore ${traits}/qcovars3_${i}.txt > ${traits}/qcovars_${i}   #SBayesfixed effect. If you run sbayesr uncomment this line and comment the next one. 
+	#awk 'NR==FNR{a[$1]=$2;next}{print $0,a[$1] ? a[$1] : "NA"}' ${Pred}/${TRAIT}_pred_auto_LOCO_${i}.PRS.out ${traits}/qcovars3_${i}.txt > ${traits}/qcovars_${i} # If using LDpred2 uncomment and comment the next line    
 	awk 'NR==FNR{a[$1]=$3;next}{print $0,a[$1] ? a[$1] : "NA"}' ${PGS}/PGS.${i}.all.score ${traits}/qcovars3_${i}.txt > ${traits}/qcovars_${i}  # This line include PRSice LOCO-PGS
 	var="FID IID PC1 PC2 PC3 PC4 PC5 PC6 PC7 PC8 PC9 PC10 age batch PGS"
 	sed -i "1s/.*/$var/" ${traits}/qcovars_${i}
