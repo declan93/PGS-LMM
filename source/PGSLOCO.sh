e1afa0220315bede533003132c08edf862aea8d2
Declan Bennett <declanbennett@hotmail.com>
2021-04-27 12:20:49 +0100

    Update PGSLOCO.sh

--- source/PGSLOCO.sh
+++ source/PGSLOCO.sh
@@ -11,4 +11,4 @@ source "${PWD}/config.txt"
 ${PRSice} --thread 8 --base ${results}/prsStat_${SLURM_ARRAY_TASK_ID}.gz --chr CHR --A1 A1 --A2 A2 --stat BETA --snp SNP --bp POS --pvalue P --target ${GT}/${NAME}_# --pheno-file ${PHENO} --all-score --fastscore --bar-levels 5e-05 --pheno-col ${TRAIT} --score avg --out ${PGS}/PGS.${SLURM_ARRAY_TASK_ID} --clump-p 5e-05
 
 # LDpred code for prediction
-# Rscript LDpred2.R BMI Pheno.loco.sumstats.${SLURM_ARRAY_TASK_ID}.gz ${SLURM_ARRAY_TASK_ID}
+# Rscript LDpred2.R ${TRAIT} ${results}/prsStat_${SLURM_ARRAY_TASK_ID}.gz ${SLURM_ARRAY_TASK_ID}
