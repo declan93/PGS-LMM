51fa71c51a93ea85b325bf9dc2d343bbd62b98fb
Declan Bennett <declanbennett@hotmail.com>
2021-05-18 14:56:47 +0100

    color group

--- paper/Figures/Fig1.R
+++ paper/Figures/Fig1.R
@@ -14,7 +14,7 @@ p <- ggplot(tbl, aes(x=method, y=NcausalSig/Ncausal, group=method)) + geom_boxpl
   theme_classic(base_size=10) + ylab("Proportion") + xlab("Method") + 
   theme(legend.position="none",axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
   ylim(0.4,0.6) + 
-  geom_hline(data= med_dat, aes( yintercept=med, col=V4),linetype="dotted" )
+  geom_hline(data= med_dat, aes( yintercept=med, col=method),linetype="dotted" )
 
 # save 
 ggsave(p, filename = "Fig1.png",dpi=500)
